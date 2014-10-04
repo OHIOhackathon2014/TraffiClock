d6317a1899d1e1fc19dead42eb9e50678004a14a
abbott221 <abbottm07@gmail.com>
2014-10-04 13:46:23 -0400

    NotificationManager stuff coming soon

--- src/com/mobile/android/trafficlock/utils/AlarmEventDispatcher.java
+++ src/com/mobile/android/trafficlock/utils/AlarmEventDispatcher.java
@@ -3,14 +3,18 @@ package com.mobile.android.trafficlock.utils;
 import android.app.AlarmManager;
 import android.app.IntentService;
 import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
 
 /**
  * Created by Michael Abbott on 10/4/14.
  */
 public class AlarmEventDispatcher extends IntentService {
 
+
+    public static final String ACTION = "ACTION";
 
     public AlarmEventDispatcher() {
         super("something");
@@ -21,18 +25,30 @@ public class AlarmEventDispatcher extends IntentService {
         //
     }
 
-    private void execute(String action, String notificationId) {
+    private void execute() {
         AlarmManager am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
 
-        Intent noiseEvent = new Intent();
+
+        BroadcastReceiver br = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                //
+            }
+        };
+
+        Intent noiseEvent = new Intent(this, br.getClass());
 
         long time = 0;
 
         //it's a singleton
         //PendingIntent pi = new PendingIntent();
 
+
+        //"IntentSender" is now/redirects to "PendingIntent" in the API documentation
+
+
         //one of these to retrieve an instance:
-        PendingIntent noiseEventPending = PendingIntent.getBroadcast(this, 0, new Intent(), PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent noiseEventPending = PendingIntent.getBroadcast(this, 0, noiseEvent, PendingIntent.FLAG_UPDATE_CURRENT);
         //PendingIntent pi = PendingIntent.getService();
         //get Broadcast is mentioned to be what is typically used.
 
