0532d3d1ba5c987feeb9bac476ce575bfc7a06a1
KosherBacon <j.harris.kahn@gmail.com>
2014-10-04 02:22:44 -0400

    Changes

--- src/com/mobile/android/trafficlock/SettingsActivity.java
+++ src/com/mobile/android/trafficlock/SettingsActivity.java
@@ -1,13 +1,8 @@
 package com.mobile.android.trafficlock;
 
 import android.app.ActionBar;
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 public class SettingsActivity extends PreferenceActivity {
     /**
@@ -16,6 +11,10 @@ public class SettingsActivity extends PreferenceActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ActionBar actionBar = getActionBar();
+
+        actionBar.setTitle(getResources().getString(R.string.app_name) + " Settings");
 
         addPreferencesFromResource(R.xml.preferences);
 
