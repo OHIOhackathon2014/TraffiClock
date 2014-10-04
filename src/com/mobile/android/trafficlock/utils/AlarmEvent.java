83f9bd40dbcc4bb059672944a54e5f96fac806d1
abbott221 <abbottm07@gmail.com>
2014-10-04 14:30:45 -0400

    More notification stuff, manifest updates soon

--- src/com/mobile/android/trafficlock/utils/AlarmEvent.java
+++ src/com/mobile/android/trafficlock/utils/AlarmEvent.java
@@ -1,10 +1,27 @@
 package com.mobile.android.trafficlock.utils;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
 /**
  * Created by michael on 10/4/14.
  */
-public class AlarmEvent {
+public class AlarmEvent extends BroadcastReceiver {
 
 
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
+        long[] pattern = new long[5];
+
+        Notification notify = new Notification.Builder(context)
+                .setVibrate(pattern)
+                .build();
+
+        nm.notify(0, notify);
+    }
 }
--- src/com/mobile/android/trafficlock/utils/AlarmEventDispatcher.java
+++ src/com/mobile/android/trafficlock/utils/AlarmEventDispatcher.java
@@ -2,10 +2,12 @@ package com.mobile.android.trafficlock.utils;
 
 import android.app.AlarmManager;
 import android.app.IntentService;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.IntentSender;
 
 /**
@@ -16,27 +18,44 @@ public class AlarmEventDispatcher extends IntentService {
 
     public static final String ACTION = "ACTION";
 
+    private IntentFilter matcher;
+
     public AlarmEventDispatcher() {
-        super("something");
+        super("wut");
+
+        matcher = new IntentFilter();
+        matcher.addAction(ACTION);
+    }
+
+    public AlarmEventDispatcher(String workerName) {
+        super(workerName);
+
+        matcher = new IntentFilter();
+        matcher.addAction(ACTION);
     }
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        //
-    }
 
-    private void execute() {
+        String action = intent.getAction();
+
         AlarmManager am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
 
 
-        BroadcastReceiver br = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                //
-            }
-        };
 
-        Intent noiseEvent = new Intent(this, br.getClass());
+//        BroadcastReceiver br = new BroadcastReceiver() {
+//            @Override
+//            public void onReceive(Context context, Intent intent) {
+//                NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+//                nm.notify();
+//            }
+//        };
+
+        //Intent noiseEvent = new Intent(this, br.getClass());
+
+        Intent noiseEvent = new Intent(this, AlarmEvent.class);
+
+
 
         long time = 0;
 
@@ -52,6 +71,7 @@ public class AlarmEventDispatcher extends IntentService {
         //PendingIntent pi = PendingIntent.getService();
         //get Broadcast is mentioned to be what is typically used.
 
-        //am.set(AlarmManager.RTC_WAKEUP, time, pi);
+        am.set(AlarmManager.RTC_WAKEUP, time, noiseEventPending);
     }
+
 }
